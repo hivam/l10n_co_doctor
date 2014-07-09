126f1a620e86ca37eb5a44da238445118239a5c2
hivam <quipus.total@gmail.com>
2014-07-09 12:05:57 -0500

    Agregando impresión Historia Clínica
    Se agrega formato de impresión de la Historia Clínica de la Atención.

--- report/doctor_attention.py
+++ report/doctor_attention.py
@@ -17,13 +17,23 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-##############################################################################
-import doctor_precription
-import doctor_clinical_laboratory
-import doctor_diagnostic_images
-import doctor_surgical
-import doctor_therapeutic
-import doctor_other_procedures
-import doctor_referral
-import doctor_disability
-import doctor_attention
+###############################################################################
+
+import time
+from openerp.report import report_sxw
+from openerp import pooler
+
+class doctor_attention(report_sxw.rml_parse):
+    def __init__(self, cr, uid, name, context):
+        super(doctor_attention, self).__init__(cr, uid, name, context=context)
+        self.localcontext.update({
+                                  'time': time,                              
+                                  })        
+    
+report_sxw.report_sxw('report.doctor_attention', 'doctor.attentions',
+                      'addons/l10n_co_doctor/report/doctor_attention.rml',
+                      parser=doctor_attention)
+        
+        
+        
+        
