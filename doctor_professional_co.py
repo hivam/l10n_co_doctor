dceb6525241b0687e013de56b13c3b9f0faa161c
Jose_sebastian <michu-1@hotmail.com>
2015-05-20 16:31:43 -0500

    integracion profesional salud/tercero -Capriatto

--- doctor_professional_co.py
+++ doctor_professional_co.py
@@ -18,7 +18,8 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-
+import logging
+_logger = logging.getLogger(__name__)
 import openerp
 import re
 import codecs
@@ -33,6 +34,7 @@ class doctor_professional_co(osv.osv):
     _description='Inherited Modelsd to add fields.'
     _name ='doctor.professional'
     _inherit ='doctor.professional'
+
     _columns = {
         'tdoc': fields.selection((('11','Registro civil'), ('12','Tarjeta de identidad'),
                                   ('13','Cédula de ciudadanía'), ('21','Tarjeta de extranjería'),
@@ -47,8 +49,21 @@ class doctor_professional_co(osv.osv):
         'firtsname': fields.char('Primer Nombre', size=25, required=True),
         'middlename': fields.char('Segundo Nombre', size=25),
         'nombreUsuario': fields.char('Nombre Usuario', size=40, required=True),
+        'state_id' : fields.many2one('res.country.state', 'Departamento', required=False),
+        'city_id' : fields.many2one('res.country.state.city', 'Ciudad', required=False , domain="[('state_id','=',state_id)]"),
+        'street' :  fields.char('Dirección', required=False),
+        'zona':  fields.selection ((('U','Urbana'), ('R','Rural')), 'Zona de residencia', required=True),
 
     }
+
+    def name_get(self, cr, uid, ids, context={}):
+        if not len(ids):
+            return []
+        rec_name = 'nombreUsuario'
+        res = [(r['id'], r[rec_name][1])
+            for r in self.read(cr, uid, ids, [rec_name], context)]
+        return res
+
     # Función para validar que la identificación sea sólo numerica
     # Function to validate the numerical identification is only
     def _check_ident_num(self, cr, uid, ids, context=None):
@@ -64,11 +79,8 @@ class doctor_professional_co(osv.osv):
     # Function to validate that the identification is more than 6 and less than 11 digits
     def _check_ident(self, cr, uid, ids, context=None):
         for record in self.browse(cr, uid, ids, context=context):
-            # Si utiliza la direccion de la Empresa el ref viene vacio.
-            # Evitar esto con break al for.
-            if record.use_parent_address:
-                break
-            else:
+
+            if record.ref:
                 ref = record.ref
                 if not ref:
                     return True
@@ -88,6 +100,22 @@ class doctor_professional_co(osv.osv):
             elif ref_ids:
                 return False
         return True
+
+    def create(self, cr, uid, vals, context=None):
+        vals.update({'name' : "%s %s %s %s" % (vals['lastname'] , vals['surname'] or '' , vals['firtsname'] , vals['middlename'] or '')})
+        vals.update({'nombreUsuario': vals['name'].upper()})
+        vals.update({'username': vals['name'].upper()})
+        if vals['middlename']:
+            vals.update({'middlename': vals['middlename'].upper() })
+        if vals['surname']:
+            vals.update({'surname': vals['surname'].upper() })
+        vals.update({'lastname': vals['lastname'].upper() })
+        vals.update({'firtsname': vals['firtsname'].upper() })
+        vals.update({'name' : "%s %s %s %s" % (vals['lastname'] , vals['surname'] or '' , vals['firtsname'] , vals['middlename'] or '')})
+
+        self.pool.get('res.partner').create(cr, uid, {'ref': vals['ref'], 'tdoc': vals['tdoc'], 'middlename' : vals['middlename'] or '', 'surname' : vals['surname'] or '',  'lastname': vals['lastname'], 'firtsname': vals['firtsname'], 'image': vals['photo'], 'city_id': vals['city_id'], 'state_id': vals['state_id'], 'street': vals['street'], 'phone': vals['work_phone'], 'mobile': vals['work_mobile'], 'email': vals['work_email'], 'name': vals['name']}, context)
+
+        return super(doctor_professional_co, self).create(cr, uid, vals, context=context)
 
     _constraints = [
         (_check_ident, '¡Error! Número de identificación debe tener entre 6 y 11 dígitos', ['ref']),
