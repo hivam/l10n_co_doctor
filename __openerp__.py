4b1edd4affc9fa83afc4332e401350c3dbe6868b
camilozuluaga <juancamilozuluaga@hotmail.com>
2015-12-14 22:42:26 -0500

    arreglando problema con data

--- __openerp__.py
+++ __openerp__.py
@@ -62,9 +62,9 @@ Location change for "Doctor" module in Colombia
               'data/l10n_doctor_dose_unit_co_data.xml',
               'data/l10n_doctor_drugs_co.xml',
               'data/l10n_doctor_health_procedures_co.xml',
-              'data/l10n_doctor_res_partner_data.xml',
-              'data/l10n_doctor_insurer_data.xml',
-              'data/l10n_doctor_insurer_plan_data.xml',
+              #'data/l10n_doctor_res_partner_data.xml',
+              #'data/l10n_doctor_insurer_data.xml',
+              #'data/l10n_doctor_insurer_plan_data.xml',
               'doctor_report.xml',
 
     ],
