daaf0e136f3ada2ff87021087ee286b17a55562a
camilozuluaga <juancamilozuluaga@hotmail.com>
2015-12-22 18:25:49 -0500

    agregando cambios a reportes campos nuevos, y emtodos nuevos

--- report/rips_radicacioncuentas.py
+++ report/rips_radicacioncuentas.py
@@ -29,23 +29,48 @@ class rips_radicacioncuentas(report_sxw.rml_parse):
         super(rips_radicacioncuentas, self).__init__(cr, uid, name, context=context)
         self.localcontext.update({
             'time': time,
+            'core': self.core,
             'base': self.base,
             'select_codigo_procedimiento': self.select_codigo_procedimiento,
             'select_nombre_procedimiento': self.select_nombre_procedimiento,
             'select_cantidad_procedimiento': self.select_cantidad_procedimiento,
+            'base_cita': self.base_cita,
+            'select_plan_usuario': self.select_plan_usuario,
+            'select_numero_autorizacion': self.select_numero_autorizacion,
+            'base_atencion': self.base_atencion,
+            'fecha_consulta':self.fecha_consulta,
+            'total': self.total,
+            'base_diagnostico': self.base_diagnostico,
+            'diagnostico': self.diagnostico,
+            'cie10': self.cie10,
         })
 
 
-    def base(self, invoice_id):
+    def core(self):
         context = {}
         context.update({'lang' : self.pool.get('res.users').browse(self.cr, self.uid, self.uid, context=context).lang})
+
+    def base(self, invoice_id):
+        context = {}
+        self.core()
         cuenta_line = self.pool.get('account.invoice.line')
         return cuenta_line.search(self.cr, self.uid, [('invoice_id', '=', invoice_id)], context=context)    
 
     
+    def total(self, fecha_desde, fecha_hasta):
+        context = {}
+        self.core()
+        cuenta = self.pool.get('account.invoice')
+        dato = 0
+        cuenta_ids = cuenta.search(self.cr, self.uid, [('date_invoice', '>=', fecha_desde), ('date_invoice', '<=', fecha_hasta), ('state', '=', 'open')], context=context)
+        for i in cuenta.browse(self.cr, self.uid, cuenta_ids, context=context):
+            dato += i.amount_total
+
+        return dato
+
     def select_codigo_procedimiento(self, invoice_id):
         context = {}
-        context.update({'lang' : self.pool.get('res.users').browse(self.cr, self.uid, self.uid, context=context).lang})
+        self.core()
         cuenta = self.pool.get('account.invoice.line')
         for i in cuenta.browse(self.cr, self.uid, self.base(invoice_id), context=context):
             dato = i.product_id.procedure_code
@@ -53,7 +78,7 @@ class rips_radicacioncuentas(report_sxw.rml_parse):
 
     def select_nombre_procedimiento(self, invoice_id):
         context = {}
-        context.update({'lang' : self.pool.get('res.users').browse(self.cr, self.uid, self.uid, context=context).lang})
+        self.core()
         cuenta = self.pool.get('account.invoice.line')
         for i in cuenta.browse(self.cr, self.uid, self.base(invoice_id), context=context):
             dato = i.product_id.name
@@ -61,13 +86,92 @@ class rips_radicacioncuentas(report_sxw.rml_parse):
 
     def select_cantidad_procedimiento(self, invoice_id):
         context = {}
-        context.update({'lang' : self.pool.get('res.users').browse(self.cr, self.uid, self.uid, context=context).lang})
+        self.core()
         cuenta = self.pool.get('account.invoice.line')
         for i in cuenta.browse(self.cr, self.uid, self.base(invoice_id), context=context):
             dato = i.quantity
         return dato
 
-        
+    def base_cita(self, refe_sale_orden):
+        context = {}
+        self.core()
+        sale_order = self.pool.get('sale.order')
+        sale_order_id = sale_order.search(self.cr, self.uid, [('name', '=', refe_sale_orden)], context=context)
+        cita = self.pool.get('doctor.appointment')
+        origin = ''
+        for i in sale_order.browse(self.cr, self.uid, sale_order_id, context=context):
+            origin = i.origin
+
+        cita_id = cita.search(self.cr, self.uid, [('number', '=', origin)], context=context)
+        return cita_id
+
+
+    def select_plan_usuario(self, refe_sale_orden):
+        context = {}
+        self.core()
+        cita = self.pool.get('doctor.appointment')
+        for i in cita.browse(self.cr, self.uid, self.base_cita(refe_sale_orden), context=context):
+            dato = i.plan_id.name
+        return dato
+
+    def select_numero_autorizacion(self, refe_sale_orden):
+        context = {}
+        self.core()
+        procedimiento_cita = self.pool.get('doctor.appointment.procedures')
+        procedimiento_cita_id = procedimiento_cita.search(self.cr, self.uid, [('appointment_id', 'in', self.base_cita(refe_sale_orden))], context=context)
+        for i in procedimiento_cita.browse(self.cr, self.uid, procedimiento_cita_id, context=context):
+            dato = i.nro_autorizacion
+        return dato
+
+    def base_atencion(self, refe_sale_orden):
+        context = {}
+        self.core()
+        cita = self.pool.get('doctor.appointment')
+        for i in cita.browse(self.cr, self.uid, self.base_cita(refe_sale_orden), context=context):
+            dato = i.number
+        return dato
+
+    def fecha_consulta(self, refe_sale_orden):
+        context = {}
+        self.core()
+        atencion = self.pool.get('doctor.attentions')
+        atencion_id = atencion.search(self.cr, self.uid, [('origin', '=', self.base_atencion(refe_sale_orden))], context=context)
+        dato = ''
+        for i in atencion.browse(self.cr, self.uid, atencion_id, context=context):
+            dato = i.date_attention
+        if dato:
+            dato = dato[0:10]
+        return dato
+
+    def base_diagnostico(self, refe_sale_orden):
+        context = {}
+        self.core()
+        atencion = self.pool.get('doctor.attentions')
+        diagnostico = self.pool.get('doctor.attentions.diseases')
+        atencion_id = atencion.search(self.cr, self.uid, [('origin', '=', self.base_atencion(refe_sale_orden))], context=context)
+        diagnostico_id = diagnostico.search(self.cr, self.uid, [('attentiont_id', 'in', atencion_id)], context=context)
+        return diagnostico_id
+
+
+    def diagnostico(self, refe_sale_orden):
+        context = {}
+        self.core()
+        diagnostico = self.pool.get('doctor.attentions.diseases')
+        dato = ''
+        for i in diagnostico.browse(self.cr, self.uid, self.base_diagnostico(refe_sale_orden), context=context):
+            dato = i.diseases_id.name
+        return dato
+
+    def cie10(self, refe_sale_orden):
+        context = {}
+        self.core()
+        diagnostico = self.pool.get('doctor.attentions.diseases')
+        dato = ''
+        for i in diagnostico.browse(self.cr, self.uid, self.base_diagnostico(refe_sale_orden), context=context):
+            dato = i.diseases_id.code
+        return dato
+
+
 report_sxw.report_sxw('report.rips_radicacioncuentas', 'rips.radicacioncuentas',
                       'addons/l10n_co_doctor/report/rips_radicacioncuentas.rml',
                       parser=rips_radicacioncuentas)
