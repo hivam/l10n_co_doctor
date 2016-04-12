d3ae7b5cc533a6185c7c833a2ed13404d408fca2
camilozuluaga <juancamilozuluagaserna@hotmail.com>
2016-04-12 13:44:26 -0500

    agregando cambios reporte

--- report/doctor_account_voucher.py
+++ report/doctor_account_voucher.py
@@ -47,7 +47,7 @@ class doctor_account_voucher(report_sxw.rml_parse):
         cuenta_line = self.pool.get('account.invoice.line')
         paciente_id = doctor_paciente.search(self.cr, self.uid, [('patient', '=', partner_id)], context=context)
         cuenta_ids = []
-        fecha = date.strftime('%d-%m-%Y')
+        fecha = datetime.strptime(fecha, "%d-%m-%Y")
         cuenta_id = cuenta.search(self.cr, self.uid, [('partner_id', '=', partner_id), ('date_invoice', '=', fecha), ('amount_patient', '=', pago_paciente)], context=context)
 
         _logger.info(partner_id)
