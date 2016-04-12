35785c56b167089efd47040c8d472cdd935405c2
camilozuluaga <juancamilozuluagaserna@hotmail.com>
2016-04-12 17:16:07 -0500

    agregando cambios de los reportes

--- report/doctor_account_voucher.py
+++ report/doctor_account_voucher.py
@@ -47,8 +47,12 @@ class doctor_account_voucher(report_sxw.rml_parse):
         cuenta_line = self.pool.get('account.invoice.line')
         paciente_id = doctor_paciente.search(self.cr, self.uid, [('patient', '=', partner_id)], context=context)
         cuenta_ids = [] 
+        cuenta_id = cuenta.search(self.cr, self.uid, [('patient_id', '=', paciente_id[0]), ('date_invoice', '=', fecha), ('amount_patient', '=', pago_paciente)], context=context)
 
-        cuenta_id = cuenta.search(self.cr, self.uid, [('patient_id', '=', paciente_id), ('date_invoice', '=', fecha), ('amount_patient', '=', pago_paciente)], context=context)
+        _logger.info(paciente_id[0])
+        _logger.info(fecha)
+        _logger.info(pago_paciente)
+        _logger.info(cuenta_id)
 
         for i in cuenta.browse(self.cr, self.uid, cuenta_id, context=context):
             cuenta_ids.append(i.id)
