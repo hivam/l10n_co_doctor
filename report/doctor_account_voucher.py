6165481bb4a7e72460934087a0763182a3f5178a
camilozuluaga <juancamilozuluagaserna@hotmail.com>
2016-04-13 20:18:16 -0500

    agregando cambios

--- report/doctor_account_voucher.py
+++ report/doctor_account_voucher.py
@@ -48,7 +48,7 @@ class doctor_account_voucher(report_sxw.rml_parse):
         paciente_id = doctor_paciente.search(self.cr, self.uid, [('patient', '=', partner_id)], context=context)
         cuenta_ids = [] 
         fecha = fecha.replace('/', '-')
-        cuenta_id = cuenta.search(self.cr, self.uid, [('patient_id', '=', paciente_id[0]), ('date_invoice', '>=', fecha), ('amount_patient', '=', pago_paciente)], context=context)
+        cuenta_id = cuenta.search(self.cr, self.uid, [('patient_id', '=', paciente_id[0]), ('date_invoice', '=', fecha)], context=context)
 
         for i in cuenta.browse(self.cr, self.uid, cuenta_id, context=context):
             cuenta_ids.append(i.id)
