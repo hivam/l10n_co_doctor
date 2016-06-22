a3842ebdf7105e60be81ae679006e6aa29110b0d
BrayhanJaramillo <brayhanjaramillo@gmail.com>
2016-06-22 18:52:24 -0500

    bug

--- __openerp__.py
+++ __openerp__.py
@@ -67,7 +67,7 @@ Location change for "Doctor" module in Colombia
               'data/l10n_doctor_res_partner_data.xml',
               'data/l10n_doctor_insurer_data.xml',
               'data/l10n_doctor_insurer_plan_data.xml',
-              #'data/l10n_doctor_paraclinical_monitoring.xml',
+              'data/l10n_doctor_paraclinical_monitoring.xml',
               'data/l10n_doctor_co_programas_academicos.xml',
               'doctor_report.xml',
 
