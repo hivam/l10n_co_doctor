8722986c5f5ae3222c094da7d58e3cc15ccf0886
BrayhanJaramillo <brayhanjaramillo@gmail.com>
2016-07-01 15:23:16 -0500

    Agregando data de dependencias de empleado, agregando onchange en empleado, docente, otros, estudiante, agregando permisos a dependencia, y agregando dato a ocupacion

--- doctor_co.py
+++ doctor_co.py
@@ -67,6 +67,14 @@ class doctor_patient_co(osv.osv):
 		('1', 'PREGRADO'),
 		('2', 'POSGRADO'),
 		('3', 'MAESTRÍAS'),
+		('4', 'ESPECIALIZACIÓN'),
+	]
+
+	#Lateralidad
+	lateralidad = [
+		('1', 'DIESTRO'),
+		('2', 'ZURDO'),
+		('3', 'AMBIDIESTRO'),
 	]
 
 	#Semestre actual
@@ -172,8 +180,11 @@ class doctor_patient_co(osv.osv):
 		'semestre_actual':fields.selection(semestre, 'Semestre Actual'),
 		'nivel_estudio':fields.selection(nivel, 'Nivel de Estudios'),
 		'programa_academico_id': fields.many2one('doctor.programa_academico', 'Programa Académico', domain="[('nivel_estudio','=',nivel_estudio)]"),
-		'es_estudiante': fields.boolean('Estudiante'),
+		'filter_ocupacion': fields.char('Filtro ocupación', size=5),
 		'neighborhood_id':fields.many2one('res.country.state.city.neighborhood', 'Barrio', required=False, domain="[('country_id','=',country_id),('state_id','=',state_id), ('city_id','=',city_id)]"),
+		'lateralidad_id':fields.selection(lateralidad, 'Lateralidad'),
+		'description_others': fields.char('Descripción', size=32),
+		'dependencia_empleado_id': fields.many2one('doctor.dependencia', 'Dependencia'),
 	}
 
 	def onchange_ocupacion_id(self, cr, uid, ids, ocupacion_id, context=None):
@@ -185,16 +196,29 @@ class doctor_patient_co(osv.osv):
 		if ocupacion_id:
 			for ocupacion in self.pool.get('doctor.patient.ocupacion').browse(cr, uid, [ocupacion_id]):
 				nombre_ocupacion= ocupacion.name
-
 		
+		_logger.info(nombre_ocupacion)
 		if nombre_ocupacion == 'Estudiante':
-			res['value']['es_estudiante'] = True  
+			res['value']['filter_ocupacion'] = 'estu'
 
-		if nombre_ocupacion != 'Estudiante':
+		if nombre_ocupacion == 'Docente':
+			res['value']['filter_ocupacion'] = 'doc'
+
+		if nombre_ocupacion == 'Egresado':
+			res['value']['filter_ocupacion'] = 'egre'
+
+		if nombre_ocupacion == 'Empleado':
+			res['value']['filter_ocupacion'] = 'emp'
+
+		if nombre_ocupacion == 'Otros':
+			res['value']['filter_ocupacion'] = 'otro'
+
+
+		if nombre_ocupacion != 'Estudiante' and nombre_ocupacion != 'Docente' and nombre_ocupacion != 'Egresado' and nombre_ocupacion != 'Empleado' and nombre_ocupacion != 'Otros':
 			res['value']['semestre_actual'] = ''  
 			res['value']['nivel_estudio']= ''
 			res['value']['programa_academico_id']= ''
-			res['value']['es_estudiante'] = False
+			res['value']['filter_ocupacion'] = ''
 
 		return res
 
@@ -338,6 +362,19 @@ class doctor_patient_co(osv.osv):
 doctor_patient_co()
 
 
+class doctor_dependencia(osv.osv):
+
+	_name= 'doctor.dependencia'
+	_rec_name='name'
+	_order= 'name'
+
+	_columns = {
+		'code':fields.char('Código', required=True),
+		'name':fields.char('Programa Académico', required=True),
+	}
+
+doctor_dependencia()
+
 #Programas academicos
 class doctor_programa_academico(osv.osv):
 
@@ -346,6 +383,7 @@ class doctor_programa_academico(osv.osv):
 		('1', 'PREGRADO'),
 		('2', 'POSGRADO'),
 		('3', 'MAESTRÍAS'),
+		('4', 'ESPECIALIZACIÓN'),
 	]
 
 	_name= 'doctor.programa_academico'
@@ -405,6 +443,8 @@ doctor_patient_co_parentesco()
 class doctor_patient_co_ocupacion(osv.Model):
 
 	_name = 'doctor.patient.ocupacion'
+	_rec_name='name'
+	_order= 'name'
 
 	_columns = {
 		'codigo' : fields.char(u'Código Ocupación' ,size = 3 ,required = False ),
@@ -3053,9 +3093,6 @@ class doctor_attentions_disability(osv.osv):
 	#Funcion para calcular los dias de incapacidad
 	def onchange_disability(self, cr, uid, ids, date_begin, date_end, context=None):
 		res={'value':{}}
-		_logger.info(date_begin)
-		_logger.info(date_end)
-		
 
 		if not date_begin:
 			raise osv.except_osv(_('Aviso Importante!'),_('Para calcular los dias de incapacidad. \n Es necesario seleccionar primero la fecha de inicio.'))
