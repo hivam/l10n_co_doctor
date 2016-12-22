31807ccf5be024fe2193a0b77d830c958d715e82
Capriatto <michu-1@hotmail.com>
2016-12-22 08:21:06 -0500

    bug fixing filtros con error / agregando filtros paciente -Capriatto

--- doctor_co.py
+++ doctor_co.py
@@ -1797,6 +1797,8 @@ class doctor_attentions_co(osv.osv):
 		'complicacion_eventoadverso' : '01',
 	}
 
+
+
 	#Funcion para cargar los seguimientos paraclinicos de acuerdo a una relacion
 	def onchange_paraclinical_monitoring(self, cr, uid, ids, seguimiento_id, patient_id, context=None):
 
