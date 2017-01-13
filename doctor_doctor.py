a75d470ab8bca6b1116ff388f239559afabc94fe
BrayhanJaramillo <brayhanjaramillo@gmail.com>
2017-01-13 11:51:24 -0500

    Bug

--- doctor_doctor.py
+++ doctor_doctor.py
@@ -84,7 +84,7 @@ class doctor(osv.osv):
 			finalidad_consulta = '04'
 		if cr.dbname == 'Tomatis':
 			finalidad_consulta = '10'
-		if cr.dbname == 'DrCaceres'
+		if cr.dbname == 'DrCaceres':
 			finalidad_consulta = '08'
 			
 		return finalidad_consulta
