d35fdf77dd0c5ac559a1384630f6393509f12c63
camilozuluaga <juancamilozuluagaserna@hotmail.com>
2017-07-14 17:09:24 -0500

    agregando cambio para guardar la finalidad de la consulta en letras al igual que la causa externa se crearon campos nuevos para no ocasionar problemas con lo que hay, y dos metodos para traducir de acuerdo el numero que es

--- doctor_doctor.py
+++ doctor_doctor.py
@@ -1,3 +1,24 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#   OpenERP, Open Source Management Solution
+#   Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as
+#   published by the Free Software Foundation, either version 3 of the
+#   License, or (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
 import logging
 from openerp.osv import fields, osv;
 from datetime import datetime, timedelta, date
@@ -165,8 +186,71 @@ class doctor(osv.osv):
 		return nombre_tipo
 
 
-	def obtener_ultimas_atenciones_paciente(self, cr, uid, modelo_buscar, tiempo, paciente, fecha_atencion, context=None):
+	def causa_externa_rips(self, numero_causa):
 
+		nombre_causa = None
+
+		if numero_causa == '01':
+			nombre_causa = 'Accidente de trabajo'
+		elif numero_causa == '02':
+			nombre_causa = u'Accidente de tránsito'
+		elif numero_causa == '03':
+			nombre_causa = u'Accidente rábico'
+		elif numero_causa == '04':
+			nombre_causa = u'Accidente ofídico'
+		elif numero_causa == '05':
+			nombre_causa = 'Otro tipo de accidente'
+		elif numero_causa == '06':
+			nombre_causa = u'Evento Catastrófico'
+		elif numero_causa == '07':
+			nombre_causa = u'Lesión por agresión'
+		elif numero_causa == '08':
+			nombre_causa = u'Lesión auto infligida'
+		elif numero_causa == '09':
+			nombre_causa = u'Sospecha de maltrato físico'
+		elif numero_causa == '10':
+			nombre_causa = 'Sospecha de abuso sexual'
+		elif numero_causa == '11':
+			nombre_causa = 'Sospecha de abuso sexual'
+		elif numero_causa == '12':
+			nombre_causa = 'Sospecha de maltrato emocional'
+		elif numero_causa == '13':
+			nombre_causa = 'Enfermedad general'
+		elif numero_causa == '14':
+			nombre_causa = 'Enfermedad profesional'
+		elif numero_causa == '15':
+			nombre_causa = 'Otra'
+		return nombre_causa
+
+
+	def finalidad_consulta_rips(self, numero_finalidad):
+
+		nombre_finalidad = None
+
+		if numero_finalidad == '01':
+			nombre_finalidad = u'Atención del parto -puerperio'
+		elif numero_finalidad == '02':
+			nombre_finalidad = u'Atención del recién nacido'
+		elif numero_finalidad == '03':
+			nombre_finalidad = u'Atención en planificación familiar'
+		elif numero_finalidad == '04':
+			nombre_finalidad = u'Detección de alteraciones del crecimiento y desarrollo del menor de diez años'
+		elif numero_finalidad == '05':
+			nombre_finalidad = u'Detección de alteración del desarrollo joven'
+		elif numero_finalidad == '06':
+			nombre_finalidad = u'Detección de alteraciones del embarazo'
+		elif numero_finalidad == '07':
+			nombre_finalidad = u'Detección de alteración del adulto'
+		elif numero_finalidad == '08':
+			nombre_finalidad = u'Detección de alteración de agudeza visual'
+		elif numero_finalidad == '09':
+			nombre_finalidad = u'Detección de enfermedad profesional'
+		elif numero_finalidad == '10':
+			nombre_finalidad = u'No aplica'
+		return nombre_finalidad
+
+	def obtener_ultimas_atenciones_paciente(self, cr, uid, modelo_buscar, tiempo, paciente, fecha_atencion, context=None):
+		'''
 		if modelo_buscar:
 
 			fecha_cita = datetime.strptime(fecha_atencion, "%Y-%m-%d %H:%M:%S")
@@ -182,7 +266,8 @@ class doctor(osv.osv):
 			
 				raise osv.except_osv(_('ATENCION !!!'),_('El paciente ya fue atendido por otro profesional en la salud'))
 				#_logger.info('asasasasas')
-
+		'''
+		pass
 	def tipo_historia(self, modelo):
 
 		if modelo == 'doctor_psychology':
